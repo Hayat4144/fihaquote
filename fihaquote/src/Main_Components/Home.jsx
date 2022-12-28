2d17a9c8ea2defb556c3fd4997cfd868cd829b0d
Hayat4144 <ihayat855@gmail.com>
2022-12-28 09:30:28 +0530

    index.css file is deleted.

--- fihaquote/src/Main_Components/Home.jsx
+++ fihaquote/src/Main_Components/Home.jsx
@@ -1,19 +1,14 @@
-import React from 'react'
-import { useContext } from 'react' ;
-import { AppContext } from '../Context/Context_State';
-
+import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../Context/Context_State";
 
 export default function Home() {
   // use of context hook
-  const context = useContext(AppContext) ;
-  const {IsAuthenticate} = context ;
+  const context = useContext(AppContext);
+  const { IsAuthenticate } = context;
 
+  // check if user is logdin or not
+  IsAuthenticate();
 
-  // check if user is logdin or not 
-  IsAuthenticate() ;
-
-  
-  return (
-    <div>This is Home page</div>
-  )
+  return <div>This is Home page</div>;
 }
